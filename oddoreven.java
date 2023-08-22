2bfab6ed3d2d8dae3696eb34494330ca07a2ac72
vinothini-ds <142780008+vinothini-ds@users.noreply.github.com>
2023-08-22 09:20:30 +0000

    completed oddoreven

---
+++ oddoreven.java
@@ -0,0 +1,11 @@
+class oddoreven{
+    public static void main(String args[]){
+        int num1=3;
+        if(num1%2!=0){
+        System.out.println("The given number is ODD");
+}
+else{
+    System.out.println("The given number is EVEN");
+}
+    }
+}
