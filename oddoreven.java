9c096f4dac7ef2edb42a2e67e8db418c81856266
vinothini-ds <142780008+vinothini-ds@users.noreply.github.com>
2023-08-22 09:24:59 +0000

    completed

--- oddoreven.java
+++ oddoreven.java
@@ -1,6 +1,6 @@
 class oddoreven{
     public static void main(String args[]){
-        int num1=3;
+        int num1=10;
         if(num1%2!=0){
         System.out.println("The given number is ODD");
 }
